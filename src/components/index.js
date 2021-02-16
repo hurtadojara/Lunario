8a2263460ec3dc291267ccc2bde656712915453c
hurtadojara <andreshurtadojaramillo@gmail.com>
2021-02-16 14:30:17 -0500

    SERVERS READY

---
+++ src/components/index.js
@@ -0,0 +1,34 @@
+const express = require('express')
+const app = express()
+let {PythonShell} = require('python-shell')
+
+    function runOauth(){
+        return PythonShell.run('./scripts_API/oauth.py', null, function (err) {
+        if (err) throw err;
+        console.log('finished');
+      });
+      }
+
+      function runEvent(){
+        return PythonShell.run('./scripts_API/create_event.py', null, function (err) {
+        if (err) throw err;
+        console.log('finished');
+      });
+      }
+      
+      app.get('/oauth', function (req, res) {
+        const subprocess = runOauth()
+        res.set('Content-Type', 'text/plain');
+        subprocess.stdout.pipe(res)
+        subprocess.stderr.pipe(res)
+      })
+
+      app.post('/createevent', function (req, res) {
+        const subprocess = runEvent()
+        res.set('Content-Type', 'text/plain');
+        subprocess.stdout.pipe(res)
+        subprocess.stderr.pipe(res)
+      });
+
+
+    app.listen(8080, () => console.log('Server running'));
