df98fb5243ba2cfe008853e64b774369218cecc8
hurtadojara <andreshurtadojaramillo@gmail.com>
2021-02-18 22:42:35 -0500

    CORS PLEASE WORK PLEASE

--- server/index.js
+++ server/index.js
@@ -4,7 +4,7 @@ const cors = require('cors')
 let {PythonShell} = require('python-shell')
 
     app.use(function(req, res, next) {
-      res.header("Access-Control-Allow-Origin", "https://localhost:3000"); // update to match the domain you will make the request from
+      res.header("Access-Control-Allow-Origin", "*"); // update to match the domain you will make the request from
       res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
       next();
     });
