ed82727616ace5cc9d2c217b69f1c0e184445b70
hurtadojara <andreshurtadojaramillo@gmail.com>
2021-03-15 18:38:49 -0500

    calendar

--- client/src/components/Cal.js
+++ client/src/components/Cal.js
@@ -1,6 +1,6 @@
 import React, { Component } from "react";
 import {Calendar, momentLocalizer} from "react-big-calendar";
-import 'react-big-calendar/lib/css/react-big-calendar.css';
+import './styles/Cal.css';
 import moment from "moment";
 
 
